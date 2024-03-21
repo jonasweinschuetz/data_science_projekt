8200002389ff83a456fb4dc86c8a73e7a0522afa
Jonas <stu222311@mail.uni-kiel.de>
2024-03-21 13:08:07 +0100

    update pages

--- pages/rq5.py
+++ pages/rq5.py
@@ -62,7 +62,7 @@ def canteen_meal_distribution_after_std_threshold(df, std_threshold, max_show=20
 
 
 #path_data = "./data/german-canteens(filtered).csv"
-path_data = "'https://media.githubusercontent.com/media/jonasweinschuetz/data_science_projekt/main/data/german-canteens(filtered).csv"
+path_data = "https://media.githubusercontent.com/media/jonasweinschuetz/data_science_projekt/main/data/german-canteens(filtered).csv"
 path_geo = './data/further_updated_german_canteens.json'
 data = pd.read_csv(path_data,sep="@",index_col=0) 
 geo_data = pd.read_json(path_geo ,encoding='utf8')
