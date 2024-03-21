8ccc9b77f7cfbbc9e1c3165353399d4f6f6265b7
jonasweinschuetz <163414661+jonasweinschuetz@users.noreply.github.com>
2024-03-21 23:39:41 +0100

    Add files via upload

---
+++ pages/cleaning.py
@@ -0,0 +1,18 @@
+import dash
+from dash import Dash, dcc, html, callback, Input, Output
+import dash_bootstrap_components as dbc
+import plotly.express as px
+import pandas as pd
+import requests
+import json
+
+dash.register_page(__name__, external_stylesheets =[dbc.themes.BOOTSTRAP])
+
+layout = dbc.Container([
+    dbc.Row([
+            dbc.Col([
+            dcc.Markdown('Hier könnte ihr data cleaning beschreibung stehen',style={'textAlign':'center'})
+        ],width = 12)
+    ]),
+    ])
+
