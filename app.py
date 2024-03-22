c742371239c6f59da2d864b8a1d414c1778c2a4d
SEESchwane <stu214570@mail.uni-kiel.de>
2024-03-22 02:25:56 +0100

    YEEEEEEEHAAAAAAAAAAAAAAAA

--- app.py
+++ app.py
@@ -4,38 +4,62 @@ import dash_bootstrap_components as dbc
 import plotly.express as px
 from dash.dependencies import Input, Output
 import pandas as pd
+import base64
 
 app = Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
 
 LETTER_STYLE ={"font-size": 14}
 
-nav_items = [
-    dbc.NavItem(dbc.NavLink("Home", href=dash.page_registry['pages.home']['path'], active="exact", style=LETTER_STYLE)),
-    dbc.NavItem(dbc.NavLink("Data collection", href=dash.page_registry['pages.collect']['path'], active="exact", style=LETTER_STYLE)),
-    dbc.NavItem(dbc.NavLink("Data cleaning", href=dash.page_registry['pages.cleaning']["path"], active="exact", style=LETTER_STYLE)),
-    dbc.NavItem(dbc.NavLink("How has the average price for a meal in Schleswig-Holstein changed since 2021??", href=dash.page_registry['pages.rq1']["path"], active="exact", style=LETTER_STYLE)),
-    dbc.NavItem(dbc.NavLink("How do average student meal prices compare according to their category?", href=dash.page_registry['pages.rq2']["path"], active="exact", style=LETTER_STYLE)),
-    dbc.NavItem(dbc.NavLink("How does the average price change for visitors in Germany since 2023?", href=dash.page_registry['pages.rq3']["path"], active="exact", style=LETTER_STYLE)),
-    dbc.NavItem(dbc.NavLink("Does the day of the week influence average meal prices?", href=dash.page_registry['pages.rq4']["path"], active="exact", style=LETTER_STYLE)),
-    dbc.NavItem(dbc.NavLink("Does the weekday influence the availability of dietary options in German canteens?", href=dash.page_registry['pages.rq5']["path"], active="exact", style=LETTER_STYLE)),
-    dbc.NavItem(dbc.NavLink("What are the most frequent words in meal names by category and geographic location?", href=dash.page_registry['pages.rq6']["path"], active="exact", style=LETTER_STYLE)),
-    dbc.NavItem(dbc.NavLink("Does a university’s location influence the average price of meals for students?", href=dash.page_registry['pages.rq7']["path"], active="exact", style=LETTER_STYLE)),
-]
+logo_path = "./data/Logo.png"
+encoded_image = base64.b64encode(open(logo_path, 'rb').read()).decode()
 
-navbar = dbc.NavbarSimple(
-    children=nav_items,
-    brand="NavbarSimple",
-    brand_href="#",
+navbar = dbc.Navbar(
+    dbc.Container([
+        dbc.Row(
+            [
+                dbc.Col(html.Img(src=f'data:image/png;base64,{encoded_image}', height="30px"), width="auto"),
+                dbc.Col(dbc.NavbarBrand(" ", className="ms-2"), align="center", width="auto"),
+                dbc.Col(
+                    dbc.Nav(
+                        [
+                            dbc.NavItem(dbc.NavLink("Home", href=dash.page_registry['pages.home']['path'])),
+                            dbc.NavItem(dbc.NavLink("Data Collection", href=dash.page_registry['pages.collect']['path'])),
+                            dbc.NavItem(dbc.NavLink("Data Cleaning", href=dash.page_registry['pages.cleaning']['path'])),
+                            dbc.DropdownMenu(
+                                children=[
+                                    dbc.DropdownMenuItem("How has the average price for a meal in Schleswig-Holstein changed since 2021?", href=dash.page_registry['pages.rq1']['path']),
+                                    dbc.DropdownMenuItem("How do average student meal prices compare according to their category?", href=dash.page_registry['pages.rq2']['path']),
+                                    dbc.DropdownMenuItem("How does the average price change for visitors in Germany since 2023?", href=dash.page_registry['pages.rq3']['path']),
+                                    dbc.DropdownMenuItem("Does the day of the week influence average meal prices?", href=dash.page_registry['pages.rq4']['path']),
+                                    dbc.DropdownMenuItem("Does the weekday influence the availability of dietary options in German canteens?", href=dash.page_registry['pages.rq5']['path']),
+                                    dbc.DropdownMenuItem("What are the most frequent words in meal names by category and geographic location?", href=dash.page_registry['pages.rq6']['path']),
+                                    dbc.DropdownMenuItem("Does a university’s location influence the average price of meals for students?", href=dash.page_registry['pages.rq7']['path']),
+                                ],
+                                nav=True,
+                                in_navbar=True,
+                                label="Research Questions",
+                            ),
+                        ],
+                        className="ms-auto",  # This class aligns the nav items to the right
+                        navbar=True,
+                    ),
+                    width="auto",
+                ),
+            ],
+            align="center",
+        ),
+    ]),
     color="primary",
     dark=True,
+    className="mb-5",  # Adds space below the navbar
 )
 
 app.layout = dbc.Container(
     [
         navbar,
         dbc.Row([
-            dbc.Col([dash.page_container], width=6)
+            dbc.Col([dash.page_container], width=12)
         ])
     ],
     className="dbc",
